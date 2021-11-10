85875f19dccec1e15e883fce3dff90c46f7b35ba
Binay9 <soundofpeace.bd@gmail.com>
2021-11-10 16:54:20 +0000

    initate navbar & props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-        <h1>HEllo react</h1>
-    </div>
+    <>
+        {/* <Navbar home="Home props" about="About props"  />
+         */}
+         <Navbar home="Home"/>
+    </>
   );
 }
 
